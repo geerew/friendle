4b9ef009286ceba6a3d97b3fc0b42308a2fd478e
Michael Bell <michaelrbell@ymail.com>
2026-05-29 19:34:08 +0100

    cleanup utils.go and errors.go

--- utils/errors.go
+++ utils/errors.go
@@ -6,16 +6,7 @@ import "errors"
 
 var (
 	// Generic
-	ErrNilPtr        = errors.New("nil pointer")
-	ErrNotPtr        = errors.New("requires a pointer")
-	ErrNotModeler    = errors.New("does not implement Modeler interface")
-	ErrEmbedded      = errors.New("embedded struct does not implement Definer interface")
-	ErrInvalidValue  = errors.New("invalid value")
-	ErrInvalidColumn = errors.New("invalid column")
-	ErrInvalidPluck  = errors.New("pluck is only valid when selecting a single column")
-	ErrNotStruct     = errors.New("not a struct")
-	ErrNotSlice      = errors.New("not a slice")
-	ErrNoTable       = errors.New("table name cannot be empty")
+	ErrNilPtr = errors.New("nil pointer")
 
 	// API
 	ErrApiQueryParse = errors.New("list query parse error")
@@ -25,29 +16,8 @@ var (
 	ErrPrincipal = errors.New("principal not found in context")
 
 	// Model
-	ErrId                  = errors.New("id cannot be empty")
-	ErrCourseId            = errors.New("course id cannot be empty")
-	ErrCourseNotFound      = errors.New("course not found")
-	ErrLessonId            = errors.New("lesson id cannot be empty")
-	ErrKey                 = errors.New("key cannot be empty")
-	ErrUsername            = errors.New("username cannot be empty")
-	ErrUserPassword        = errors.New("user password cannot be empty")
-	ErrLogMessage          = errors.New("log message cannot be empty")
-	ErrUserId              = errors.New("user id cannot be empty")
-	ErrAssetId             = errors.New("asset id cannot be empty")
-	ErrTag                 = errors.New("tag cannot be empty")
-	ErrTitle               = errors.New("title cannot be empty")
-	ErrPrefix              = errors.New("prefix cannot be empty or less than zero")
-	ErrPath                = errors.New("path cannot be empty")
-	ErrAssetCourseRelation = errors.New("asset does not belong to course")
-
-	// Media
-	ErrInvalidFFProbePath = errors.New("ffprobe path is invalid")
-	ErrFFProbeNotFound    = errors.New("ffprobe not found in path")
-	ErrFFProbeUnavailable = errors.New("ffprobe unavailable")
-	ErrFFProbePathEmpty   = errors.New("ffprobe path cannot be empty")
-	ErrInvalidFFmpegPath  = errors.New("ffmpeg path is invalid")
-	ErrFFmpegNotFound     = errors.New("ffmpeg not found in path")
-	ErrFFmpegUnavailable  = errors.New("ffmpeg unavailable")
-	ErrFFmpegPathEmpty    = errors.New("ffmpeg path cannot be empty")
+	ErrId           = errors.New("id cannot be empty")
+	ErrUsername     = errors.New("username cannot be empty")
+	ErrUserPassword = errors.New("user password cannot be empty")
+	ErrUserId       = errors.New("user id cannot be empty")
 )
--- utils/utils.go
+++ utils/utils.go
@@ -1,76 +1,6 @@
 package utils
 
-import (
-	"encoding/base64"
-	"fmt"
-	"net/url"
-	"os"
-	"path/filepath"
-	"runtime"
-	"strings"
-)
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// NormalizeWindowsDrive normalizes a given paths that start with a drive letter (e.g. "C:")
-// are correctly interpreted on Windows systems, by appending a backslash (\)
-//
-// For example, "C:" becomes "C:\" and "C:folder" becomes "C:\folder"
-//
-// Skipped on non-Windows platforms
-func NormalizeWindowsDrive(path string) string {
-	if runtime.GOOS == "windows" {
-		if len(path) >= 2 && path[1] == ':' {
-			if len(path) == 2 {
-				path += `\`
-			} else if path[2] != '\\' {
-				path = path[:2] + `\` + path[2:]
-			}
-		}
-	}
-
-	return path
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// GetEnvOr returns the value of the environment variable if set, otherwise returns the
-// default
-func GetEnvOr(env string, def string) string {
-	out := os.Getenv(env)
-	if out == "" {
-		return def
-	}
-
-	return out
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// DecodeString decodes a base64 encoded string
-func DecodeString(p string) (string, error) {
-	bytePath, err := base64.StdEncoding.DecodeString(p)
-	if err != nil {
-		return "", fmt.Errorf("failed to decode path")
-	}
-
-	decodedPath, err := url.QueryUnescape(string(bytePath))
-	if err != nil {
-		return "", fmt.Errorf("failed to unescape path")
-
-	}
-
-	return decodedPath, nil
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// EncodeString encodes a string into base64
-func EncodeString(p string) string {
-	encodedPath := url.QueryEscape(p)
-	res := base64.StdEncoding.EncodeToString([]byte(encodedPath))
-	return res
-}
+import "strings"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -83,29 +13,6 @@ func Map[T, V any](ts []T, fn func(T) V) []V {
 	}
 
 	return result
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// IsCard returns true when the filename is `card.[valid-ext]`
-func IsCard(filename string) bool {
-	ext := strings.ToLower(strings.TrimPrefix(filepath.Ext(filename), "."))
-
-	if ext == "" {
-		return false
-	}
-
-	name := strings.TrimSuffix(filename, "."+ext)
-	if name != "card" {
-		return false
-	}
-
-	switch ext {
-	case "jpg", "jpeg", "png", "webp", "tiff":
-		return true
-	default:
-		return false
-	}
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
