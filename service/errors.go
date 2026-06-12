10c38d44e48a9aa008fd5a5b974a05fd24667896
Michael Bell <michaelrbell@ymail.com>
2026-06-12 20:53:56 +0100

    backend: auth rework

--- service/errors.go
+++ service/errors.go
@@ -28,7 +28,6 @@ var (
 	ErrGroupJoinRequestRejected = errors.New("join request was rejected")
 	ErrGroupJoinRequestNotFound = errors.New("join request not found")
 	ErrGroupMemberNotFound      = errors.New("group member not found")
-	ErrGroupMemberSelf          = errors.New("cannot modify your own group membership")
 	ErrGroupLastAdmin           = errors.New("unable to remove the last group admin")
 	ErrGroupTooFewMembers       = errors.New("group does not have enough members to play")
 	ErrRoundNotFound            = errors.New("round not found")
